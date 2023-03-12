df8010fa8c6e1d01d703810301ffc9c3ea187ca5
Jamie Ullman <jamieMullman@gmail.com>
2023-03-12 16:18:43 -0700

    debugging fft.h fixed_mul

--- relevant-labs/microphone-cs240lx-17-i2s/code/fft.h
+++ relevant-labs/microphone-cs240lx-17-i2s/code/fft.h
@@ -2,15 +2,35 @@
 #define FFT_H
 
 #include "rpi.h"
+// #include "libc/bit-support.h"
+#define MAX_31 2147483648  // 2^31
 
 // int16 * int16 -> int32 multiply -- specific to arm_none_eabi
 inline int32_t fft_fixed_mul(int16_t a, int16_t b) {
-    unimplemented();
+    //a: r0
+    //b: r1
+    // SMLABB // bottom bottom
+    int32_t ret = 0;
+    // dev_barrier();  // multiplier is different device I suppose
+    asm volatile("SMULBB r2, r0, r1");
+    // dev_barrier();
+    asm volatile("mov r0, r2" : "=r" (ret));
+    
+    return ret;
 }
 
 // converts uint32 centered at INT_MAX into int16 (Q.15) centered at 0
 inline int16_t to_q15(uint32_t x) {
-    unimplemented();
+    // keeping: X---------------XX00000000000000
+    // x = x >> 1; 
+    unsigned mask = 1 << 31;
+    x = x & ~mask; // we want to lose the MSB because it will rarely change
+    int32_t signed_val = (int32_t)x; // all positive
+    signed_val -= MAX_31;  // Now we have a value ranging from [-2^31, 2^31 - 1] // for us: [-2^30, 2^30 - 1]
+    output("signed val %d", signed_val); 
+
+    // step 2 fit into 16 bits
+    return (int16_t)(signed_val >> (14 + 2)); // lose the 2 LSB + 14 0's.
 }
 
 // multiplies Q.15 * Q.15 into Q.15
