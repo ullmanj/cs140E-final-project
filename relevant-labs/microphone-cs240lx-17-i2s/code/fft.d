ee4782d5894013f81d1686dd6f37719eafd25484
Jamie Ullman <jamieMullman@gmail.com>
2023-03-21 14:58:41 -0700

    quick fix to test file

--- relevant-labs/microphone-cs240lx-17-i2s/code/fft.d
+++
@@ -1,7 +0,0 @@
-fft.o: fft.c fft.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/rpi.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/libc/demand.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/gpio.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/rpi-constants.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/your-prototypes.h \
- fft_sine_lut.h
--- relevant-labs/microphone-cs240lx-17-i2s/code/i2s.d
+++
@@ -1,7 +0,0 @@
-i2s.o: i2s.c i2s.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/rpi.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/libc/demand.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/gpio.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/rpi-constants.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/include/your-prototypes.h \
- /Users/jamie/Documents/CS140E/cs140E-final-project/libs-140e/libpi/libc/bit-support.h
